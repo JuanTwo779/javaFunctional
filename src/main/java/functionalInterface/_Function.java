b4be66dc8506f960204da1876ed4d08026b0098c
JuanTwo779 <104906032+JuanTwo779@users.noreply.github.com>
2022-11-24 23:53:05 +1100

    Comments to distinguish functions

--- src/main/java/functionalInterface/_Function.java
+++ src/main/java/functionalInterface/_Function.java
@@ -6,6 +6,7 @@ import java.util.function.Function;
 public class _Function {
 
     public static void main(String[] args){
+        // Function; 1 argument and produce 1 result
         int increment = incrementOne(1);
         System.out.println(increment);
 
@@ -16,18 +17,21 @@ public class _Function {
         System.out.println(squared);
 
 
-        Function<Integer, Integer> decrementThenSquare =
-                decrementOneFunction.andThen(square);
-
-        //chaining functions
+        // Chaining functions
+        Function<Integer, Integer> decrementThenSquare = decrementOneFunction.andThen(square);
         System.out.println(decrementThenSquare.apply(16));
+
+        // BiFunction ; 2 arguments and produce 1 result
+        System.out.println(incrementThenMultiply.apply(7,2));
     }
 
+    //Imperative
     private static int incrementOne(int number){
         return number + 1;
     }
     private static int incrementAndMultiply(int num, int num2){ return (num +1) * num2; }
 
+    //Declarative
     private static Function<Integer, Integer> decrementOneFunction = num -> num -1;
 
     private static Function<Integer, Integer> square = num -> num * num;
